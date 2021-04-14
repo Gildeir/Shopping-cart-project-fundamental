8fbe891dafd4233d535851f2be74f546c026c1fa
Gildeir <gildeirlopes@gmail.com>
2021-04-14 15:14:16 -0300

    Organizando slint para testes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = function onload() {};
-console.log('Its alive')
+console.log('Its alive');
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -7,6 +7,7 @@ function createProductImageElement(imageSource) {
   img.src = imageSource;
   return img;
 }
+createProductImageElement();
 
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
@@ -18,7 +19,7 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({
   sku,
   name,
-  image
+  image,
 }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -30,23 +31,24 @@ function createProductItemElement({
 
   return section;
 }
+createProductItemElement();
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({
-  sku,
-  name,
-  salePrice
-}) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({
+//   sku,
+//   name,
+//   salePrice,
+// }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
