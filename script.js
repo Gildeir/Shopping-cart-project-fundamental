cd26c9e607e1947480dbf172fa5e6a8496838170
Gildeir <gildeirlopes@gmail.com>
2021-04-16 12:27:16 -0300

    Corrigindo lint

--- script.js
+++ script.js
@@ -15,11 +15,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createCartItemElement({
-  sku,
-  name,
-  salePrice,
-}) {
+function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
